6870bf1f5c4b6f78106e6d7fe8541862755f986b
Leior Varon <leiorvaron@yahoo.com>
2025-06-06 23:14:19 -0700

    Updated the form to ask the relevant questions. Still need to improve formatting.

--- index.js
+++ index.js
@@ -4,17 +4,11 @@ const submitForm = function submitFormUserInput() {
   form.addEventListener("submit", function handleSubmit(event) {
     event.preventDefault();
 
-    if (!checkField(form.elements.fname.value, "name")) return;
-    if (!checkField(form.elements.uname.value, "username")) return;
-    if (!checkField(form.elements.email.value, "email")) return;
-    if (!checkField(form.elements.password.value, "password")) return;
-
     console.log("Name: " + form.elements.fname.value);
-    console.log("Username: " + form.elements.uname.value);
     console.log("Email: " + form.elements.email.value);
-    console.log("Password: " + form.elements.password.value);
-    console.log("Date: " + form.elements.date.value);
-    console.log("Prefered Pronouns: " + form.elements.section.value);
+    console.log("Subject: " + form.elements.subject.value);
+    console.log("Message: " + form.elements.message.value);
+    console.log("Found via: " + form.elements.section.value);
   });
 };
 
